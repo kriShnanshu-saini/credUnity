9e28f465f85bcc753ff27ef21cef85347c576362
anshuL.saini <krishnanshusaini11@gmail.com>
2024-11-21 01:20:07 +0530

    add navbar component

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,16 @@ import App from './App.jsx';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Register from '@/components/Register';
 import Login from '@/components/Login';
+import Navbar from '@/components/Navbar';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
 		<BrowserRouter>
+			<Navbar/>
 			<Routes>
-				<Route path='/' element={<App />}>
-					{/* child routes */}
-					<Route path='/auth/login' element={<Login/>}></Route>
-					<Route path='/auth/create-account' element={<Register/>}></Route>
-				</Route>
+				<Route path='/' element={<App />}></Route>
+				<Route path='/auth/login' element={<Login/>}></Route>
+				<Route path='/auth/create-account' element={<Register/>}></Route>
 			</Routes>
 		</BrowserRouter>
 	</StrictMode>
